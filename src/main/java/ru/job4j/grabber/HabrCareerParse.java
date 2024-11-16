8e1d8f0201a21acea4492a5886f55bad614103c7
itlazykin <dzaka89@gmail.com>
2024-11-16 10:32:09 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -7,6 +7,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.List;
 
 public class HabrCareerParse {
     private static final String SOURCE_LINK = "https://career.habr.com";
@@ -15,8 +16,9 @@ public class HabrCareerParse {
     public static final int PAGES = 5;
 
     public static void main(String[] args) throws IOException {
+        HabrCareerParse parser = new HabrCareerParse();
         /* формируем ссылку с учетом номера страницы и получаем саму страницу, чтобы с ней можно было работать */
-        for (int i = 0; i < PAGES; i++) {
+        for (int i = 1; i <= PAGES; i++) {
             String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, i, SUFFIX);
             Connection connection = Jsoup.connect(fullLink);
             Document document = connection.get();
@@ -29,17 +31,45 @@ public class HabrCareerParse {
         или же через селектор - select(".vacancy-card__title").
          */
             rows.forEach(row -> {
-                Element titleElement = row.select(".vacancy-card__title").first();
-                Element linkElement = titleElement.child(0);
-                Element dateElement = row.select(".vacancy-card__date").first();
-            /*
-            Получаем данные непосредственно. text() возвращает все содержимое элемента в виде текста,
-            т.е. весь текст что находится вне тегов HTML. Ссылка находится в виде атрибута,
-            поэтому ее значение надо получить как значение атрибута. Для этого служит метод attr()
-             */
-                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                System.out.printf("%s %s %s %n", dateElement.text(), titleElement.text(), link);
+                try {
+                    Element titleElement = row.select(".vacancy-card__title").first();
+                    Element linkElement = titleElement.child(0);
+                    Element dateElement = row.select(".vacancy-card__date").first();
+                    /*
+                    Получаем данные непосредственно. text() возвращает все содержимое элемента в виде текста,
+                    т.е. весь текст что находится вне тегов HTML. Ссылка находится в виде атрибута,
+                    поэтому ее значение надо получить как значение атрибута. Для этого служит метод attr()
+                    */
+                    String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                    System.out.printf(
+                            "Дата: %s | Название: %s | Ссылка: %s%nОписание: %s%n%n",
+                            dateElement.text(), titleElement.text(), link, parser.retrieveDescription(link));
+                } catch (Exception e) {
+                    System.out.println("Не удалось загрузить описание для вакансии: " + e.getMessage());
+                }
             });
         }
+    }
+
+    /**
+     * Загружает и извлекает описание вакансии по указанной ссылке.
+     * <p>
+     * Метод использует библиотеку Jsoup для выполнения HTTP-запроса и
+     * парсинга HTML-документа. Он ищет элементы с классом
+     * "vacancy-description__text" на веб-странице, извлекает текст из всех найденных
+     * элементов и объединяет его в одну строку.
+     * </p>
+     *
+     * @param link URL-адрес страницы вакансии, откуда нужно извлечь описание.
+     * @return Строка, содержащая описание вакансии, объединённое из всех текстовых блоков с классом "vacancy-description__text".
+     * @throws IOException если произошла ошибка при подключении к странице или при загрузке HTML-документа.
+     */
+    private String retrieveDescription(String link) throws IOException {
+        Connection connection = Jsoup.connect(link);
+        Document document = connection.get();
+        List<String> description = document.select(".vacancy-description__text").eachText();
+        StringBuilder stringBuilder = new StringBuilder();
+        description.forEach(stringBuilder::append);
+        return stringBuilder.toString();
     }
 }
