619dab12fcb9c1adca8bb8cbc4b3260d2771c880
itlazykin <dzaka89@gmail.com>
2024-11-17 13:33:38 +0300

    5. PsqlStore [#285209 #522141]. Правки по задаче.

---
+++ src/main/java/ru/job4j/grabber/PsqlPostStore.java
@@ -0,0 +1,112 @@
+package ru.job4j.grabber;
+
+import ru.job4j.quartz.AlertRabbit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlPostStore implements Store {
+    private Connection connection;
+    private static final Logger LOG = LoggerFactory.getLogger(PsqlPostStore.class.getName());
+
+    public PsqlPostStore(Properties config) throws SQLException {
+        try {
+            Class.forName(config.getProperty("driver-class-name"));
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        connection = DriverManager.getConnection(
+                config.getProperty("url"),
+                config.getProperty("username"),
+                config.getProperty("password")
+        );
+    }
+
+    @Override
+    public void save(Post post) {
+        String sql = "INSERT INTO post (name, text, link, created) VALUES (?, ?, ?, ?) ON CONFLICT DO NOTHING";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, post.getTitle());
+            preparedStatement.setString(2, post.getDescription());
+            preparedStatement.setString(3, post.getLink());
+            preparedStatement.setTimestamp(4, Timestamp.valueOf(post.getCreated()));
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            LOG.error("Something wrong with request to database ", e);
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        List<Post> allPosts = new ArrayList<>();
+        String sql = "SELECT * FROM post";
+        try (Statement statement = connection.createStatement()) {
+            try (ResultSet resultSet = statement.executeQuery(sql)) {
+                while (resultSet.next()) {
+                    allPosts.add(getPost(resultSet));
+                }
+            }
+        } catch (SQLException e) {
+            LOG.error("Something wrong with request to database ", e);
+        }
+        return allPosts;
+    }
+
+    @Override
+    public Post findById(int id) {
+        Post post = null;
+        String sql = "SELECT * FROM post WHERE id = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, id);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    post = getPost(resultSet);
+                }
+            }
+        } catch (SQLException e) {
+            LOG.error("Something wrong with request to database ", e);
+        }
+        return post;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    private Post getPost(ResultSet resultSet) throws SQLException {
+        Post post = new Post();
+        post.setId(resultSet.getInt("id"));
+        post.setTitle(resultSet.getString("name"));
+        post.setLink(resultSet.getString("link"));
+        post.setDescription(resultSet.getString("text"));
+        post.setCreated(resultSet.getTimestamp("created").toLocalDateTime());
+        return post;
+    }
+
+    public static void main(String[] args) {
+        Post test = new Post();
+        test.setTitle("Title");
+        test.setDescription("Text");
+        test.setLink("some link");
+        test.setCreated(LocalDateTime.now());
+        System.out.println(test);
+        try (Store psqlPostStore = new PsqlPostStore(AlertRabbit.readProps("rabbit.properties"))) {
+            psqlPostStore.save(test);
+            List<Post> posts = psqlPostStore.getAll();
+            System.out.println(posts);
+            Post post = psqlPostStore.findById(posts.get(0).getId());
+            System.out.println(post);
+            System.out.println(test.getLink().equals(post.getLink()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/ru/job4j/grabber/Store.java
+++ src/main/java/ru/job4j/grabber/Store.java
@@ -5,7 +5,7 @@ import java.util.List;
 /**
  * Проект будет хранить данные в базе Postgresql. Связь с базой будет осуществляться через интерфейс Store.
  */
-public interface Store {
+public interface Store extends AutoCloseable {
     void save(Post post);
 
     List<Post> getAll();
